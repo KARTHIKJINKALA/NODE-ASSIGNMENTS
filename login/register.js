5e6dfbaffc16ba9780b4b559f5b69e4160e77e56
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-12-15 16:46:04 +0530

    msg

--- login/register.js
+++ login/register.js
@@ -3,86 +3,76 @@ var db = require("./db.js");
 var bcrypt = require("bcrypt");
 const upload = require("./multer.js");
 
-var {transporter,options} = require("./mailer.js");
+var { transporter, options } = require("./mailer.js");
 
 // console.log(transporter)
-
-
-
-
 
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.post("/register", upload.single("profilepic"), async (req, res) => {
-//   //For storing the username
-//   var username = req.body.username;
+app.post("/register", upload.single("profilepic"), async (req, res) => {
+  //For storing the username
+  var username = req.body.username;
 
-//   //For storing the file path
-// //   console.log(req.file.path);
-//   var store_path = req.file.path;
+  //For storing the file path
+  //   console.log(req.file.path);
+  var store_path = req.file.path;
 
-//   // hashing the password to store in the database
-//   var salt = 10;
-//   var hashed_password = await bcrypt.hash(req.body.password, salt);
-// //   console.log(hashed_password);
+  // hashing the password to store in the database
+  var salt = 10;
+  var hashed_password = await bcrypt.hash(req.body.password, salt);
+  //   console.log(hashed_password);
 
+  // for storing the otp we have to send the mail to the user
 
+  transporter.sendMail(options, (err, info) => {
+    if (err) {
+      res.status(500).send(err.message);
+    } else {
+      res.send("Email sent successfully!");
+    }
+  });
 
-//   // for storing the otp we have to send the mail to the user
+  // Extracting the otp
+  // console.log(options)
+  var emp = "";
+  for (i = options.text.length - 1; i >= 13; i--) {
+    emp += options.text[i];
+  }
+  // console.log(emp)
+  // reversing for original otp
+  var OTP = [];
+  for (j = emp.length - 1; j >= 0; j--) {
+    OTP += emp[j];
+  }
 
+  var original_otp = OTP;
+  console.log(original_otp);
 
-//        transporter.sendMail(options, (err, info) => {
-//             if (err) {
-//                 res.status(500).send(err.message);
-//             } else {
-//                 res.send("Email sent successfully!");
-//             }
-//         });
-
-
-//         // Extracting the otp
-//         // console.log(options)
-//         var emp=""
-//         for( i=options.text.length-1;i>=13;i--){
-//             emp+=options.text[i]
-//         }
-//         // console.log(emp)
-//         // reversing for original otp 
-//         var OTP=[]
-//         for (j=emp.length-1;j>=0;j--){
-//             OTP+=emp[j]
-//         }
-        
-//         var original_otp=OTP
-//         console.log(original_otp)
-
-
-
-//   // Creating the table and inserting the values
-//   // var create="CREATE TABLE NODEASSIGN (USERNAME VARCHAR(20),PASSWORD VARCHAR(20))"
-//   var qinsert = "INSERT INTO NODEASSIGN (USERNAME,PASSWORD,FILEPATH,OTP) VALUES(?,?,?,?)";
-//   db.query(qinsert, [username, hashed_password, store_path,original_otp], (err, data) => {
-//     if (err) {
-//       res.send(err.message);
-//     } else {
-//       res.send("Register succesfull");
-//     }
-//   });
-//   // res.send("karthik jinkala")
-// });
-
-
-
-
-
-
+  // Creating the table and inserting the values
+  // var create="CREATE TABLE NODEASSIGN (USERNAME VARCHAR(20),PASSWORD VARCHAR(20))"
+  var qinsert =
+    "INSERT INTO NODEASSIGN (USERNAME,PASSWORD,FILEPATH,OTP) VALUES(?,?,?,?)";
+  db.query(
+    qinsert,
+    [username, hashed_password, store_path, original_otp],
+    (err, data) => {
+      if (err) {
+        res.send(err.message);
+      } else {
+        res.send("Register succesfull");
+      }
+    }
+  );
+  // res.send("karthik jinkala")
+});
 
 // for login
 
 app.post("/login", (req, res) => {
+  console.log(req.body);
   var qtotal = "SELECT USERNAME,PASSWORD,FILEPATH,OTP FROM NODEASSIGN";
 
   db.query(qtotal, async (err, data) => {
@@ -90,19 +80,24 @@ app.post("/login", (req, res) => {
       res.send(err.message);
     } else {
       for (i of data) {
-        // console.log(i.USERNAME,i.PASSWORD)\\
         c = 0;
         if (i.USERNAME == req.body.username) {
           c++;
         }
       }
+
       if (c == 1) {
         console.log("user found");
         var match = await bcrypt.compare(req.body.password, i.PASSWORD);
         console.log(match);
 
         if (match) {
-          res.send("login succesful");
+          if (req.body.otp == i.OTP) {
+            console.log(i.OTP);
+            res.send("login succesful");
+          } else {
+            res.send("Invalid otp");
+          }
         } else {
           res.send("provide the correct password");
         }
