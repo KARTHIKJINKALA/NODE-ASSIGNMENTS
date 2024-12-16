f1d93c72bd3761e5e100884fead42f89f95d1266
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2024-12-16 09:03:35 +0530

    dummydata

---
+++ dummydata/dummyapi.js
@@ -0,0 +1,23 @@
+// create an api that sends response of fakestoredata based on limit and offset using express js
+var express=require("express")
+var app=express()
+var  fs=require("fs")
+
+
+app.get("/products",(req,res)=>{
+
+
+    fs.readFile("./duummy.json","utf-8",(err,data)=>{
+        if(err){
+           res.send( err.message)
+        }
+        else{
+            res.send(data)
+        }
+    })
+
+})
+var port=3010
+app.listen(port,()=>{
+    console.log("Server has been started succesfully")
+})
