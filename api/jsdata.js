cb5a4b0204028341409be2bb32dc18ff1d097707
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2025-01-28 23:08:18 +0530

    rl

---
+++ api/jsdata.js
@@ -0,0 +1,29 @@
+// create an api that sends response of fakestoredata based on limit and offset using express js
+var express=require("express")
+var app=express()
+var  fs=require("fs")
+var cors=require("cors")
+// console.log(cors())
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/",(req,res)=>{
+
+
+    fs.readFile("./file.json","utf-8",(err,data)=>{
+        if(err){
+           res.send( err.message)
+        }
+        else{
+            // console.log(data)
+            res.send(data)
+        }
+    })
+
+})
+var port=3009
+app.listen(port,()=>{
+    console.log("Server has been started succesfully")
+})
