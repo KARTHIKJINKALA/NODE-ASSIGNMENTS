66027d4ed68deb841d41caa6ab314f1e50250646
KARTHIKJINKALA <karthikjinkala11@gmail.com>
2025-01-30 21:19:54 +0530

    msg

--- MangoDb/Mangodb.js
+++ MangoDb/Mangodb.js
@@ -4,36 +4,53 @@ const app=express()
 const {dbConnect}=require("./Connect.js")
 const { default: mongoose } = require("mongoose")
 
+app.use(express.json())
+
 dbConnect()
 // creating the schema
 // Schema is the blue print of the data in outr database
-// const userSchema=new mongoose.Schema({
-//     "name":String,
-//     "salary":Number,
-//     "present_age":Number
-// })
-// const userModel=mongoose.model("userdetails",userSchema)
+const userSchema=new mongoose.Schema({
+    name:String,
+    salary:Number,
+    present_age:Number
+})
+const userModel=mongoose.model("userdetails",userSchema)
+
+
 
 
 const postsSchema=new mongoose.Schema({
-    "name":String,
-    "age":Number,
-    "email":String
+    name:String,
+    age:Number,
+    email:String
 })
 const postModel=mongoose.model("Posts",postsSchema)
 
-app.get("/posts",async(req,res)=>{
-  try{
-    let Out=await postModel.find()
-    console.log(Out)
-    res.send(Out)
-  }
-  catch(error){
-    res.status(500).send(err)
-  }
+// app.get("/posts",async(req,res)=>{
+//   try{
+//     let Out=await postModel.find()
+//     console.log(Out)
+//     res.send(Out)
+//   }
+//   catch(error){
+//     res.status(500).send(err)
+//   }
 
+// })
+
+
+app.post("/users",async(req,res)=>{
+    console.log(req.body)
+
+    let newUser= new userModel({
+        "name":req.body.name,
+        "age":req.body.age,
+        "email":req.body.email
+    })
+
+    await newUser.save()
+    res.send("data inserted succesfully")
 })
-
 
 // app.get("/",(req,res)=>{
 //     res.send("hello this is the Mongodb")
